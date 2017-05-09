73bf814ddd73ba9c785044dceace3c6920cbd805
Alex Harries <alex@greyhead.co.uk>
2017-05-09 22:16:45 +0100

    Updated Flight Centre Communication Poderoos.

--- fcl_fcuk_communication_pods/templates/communication-pod.tpl.php
+++ fcl_fcuk_communication_pods/templates/communication-pod.tpl.php
@@ -5,11 +5,50 @@
  * Creates a communication pod.
  */
 
-// Inject the links to any CSS and JS provided by the pod.
-echo $pod_css;
-echo $pod_js;
 ?>
 
-<div class="<?php print $classes; ?>">
-  <?php print $pod_html; ?>
+<div class="communication-pod-wrapper <?php print $wrapper_classes ?>">
+  <?php if (!empty($pod_css)): ?>
+    <!-- Pod CSS. -->
+    <style type="text/css"><?php print $pod_css; ?></style>
+  <?php endif; ?>
+
+  <?php if (!empty($pod_external_css)): ?>
+    <!-- External CSS files. -->
+    <?php foreach ($pod_external_css as $pod_external_css_file_url): ?>
+      <link type="text/css" rel="stylesheet" href="<?php print $pod_external_css_file_url; ?>" media="all"/>
+    <?php endforeach; ?>
+  <?php endif; ?>
+
+  <?php if (!empty($pod_js)): ?>
+    <!-- Pod JS. -->
+    <script type="text/javascript"><?php print $pod_js; ?></script>
+  <?php endif; ?>
+
+  <?php if (!empty($pod_external_js)): ?>
+    <!-- External CSS files. -->
+    <?php foreach ($pod_external_js as $pod_external_js_file_url): ?>
+      <script type="text/javascript" src="<?php print $pod_external_js_file_url; ?>"></script>
+    <?php endforeach; ?>
+  <?php endif; ?>
+
+  <div class="<?php print $classes; ?>">
+    <?php print render($pod_html); ?>
+
+    <?php if (!empty($edit_link) || !empty($delete_link)): ?>
+      <div class="actions-wrapper">
+        <?php if (!empty($edit_link)): ?>
+          <?php print render($edit_link); ?>
+        <?php endif; ?>
+
+        <?php if (!empty($edit_link) && !empty($delete_link)): ?>
+          |
+        <?php endif; ?>
+
+        <?php if (!empty($delete_link)): ?>
+          <?php print render($delete_link); ?>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+  </div>
 </div>
